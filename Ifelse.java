8f124f516c246f19e6e93d007e3f24078bb3685d
pavan4207 <143409132+pavan4207@users.noreply.github.com>
2023-09-03 22:53:06 -0700

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Ifelse
+{
+	public static void main(string args[])
+	{
+		int a;
+		Scanner s=new Scanner(System .in);
+		System.out.print\n("Enter a number");
+		a=s.nextint();
+		if(a%2==0)
+		{
+			System.out.println(+a" is an Even number");
+			}
+			else
+			{
+			System.out.println(+a" is an add number");
+			}
+		}
+}
