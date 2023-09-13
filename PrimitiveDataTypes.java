142197b47c3cd611d90ad73b1c71bbd3efae63d2
pavan4207 <143409132+pavan4207@users.noreply.github.com>
2023-09-12 22:03:51 -0700

    Create PrimitiveDataTypes.java

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,21 @@
+public class PrimitiveDataTypes {
+    public static void main(String args[]) {
+      boolean b=false;
+      char c='0';
+      byte by=0;
+      short s=0;
+      int i=0;
+      long l=0;
+      float f=0;
+      double d=0;
+      
+      System.out.println("boolean default value = "+b);
+       System.out.println("char default value = "+c);
+        System.out.println("byte default value = "+by);
+         System.out.println("short default value = "+s);
+          System.out.println("int default value = "+i);
+           System.out.println("long default value = "+l);
+            System.out.println("float default value = "+f);
+             System.out.println("double default value = "+d);
+    }
+}
